50c662875182721003a3b7668445a095560bb21c
Antonio Vela <antonio.vela@plainconcepts.com>
2024-01-10 15:17:52 +0100

    cambios en string

--- azureproject/production.py
+++ azureproject/production.py
@@ -7,7 +7,8 @@ cred = DefaultAzureCredential()
 accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
 
 conn_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str = psycopg2.connect(conn_string + ' password=' + accessToken.token) 
+
+conn_str = conn_string + 'password=' + accessToken.token
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
