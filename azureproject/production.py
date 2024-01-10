05bfbd307ecf028863a9555c9f4007afd1e5fd94
Antonio Vela <antonio.vela@plainconcepts.com>
2024-01-10 15:27:23 +0100

    cambio

--- azureproject/production.py
+++ azureproject/production.py
@@ -8,16 +8,14 @@ accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default
 
 conn_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 
-conn_str = conn_string + 'password=' + accessToken.token
-
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 # Uncomment the following lines for App Service
-#conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
      dbuser=conn_str_params['user'],
-     dbpass=conn_str_params['password'],
+     dbpass=accessToken.token,
      dbhost=conn_str_params['host'],
      dbname=conn_str_params['dbname']
  )
